8c853ac77ab5b44d369208f887f18f45092b6e67
Cris Pergel <cris-p>
2019-09-08 15:26:37 -0400

    Implemented splitter (preview boundary) double-click, to automatically set it square.

--- Fontogrammer/MainWindow.xaml.cs
+++ Fontogrammer/MainWindow.xaml.cs
@@ -24,5 +24,62 @@ namespace PergleLabs.UI
         {
             InitializeComponent();
         }
+
+        private void GridSplitter_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // set square
+            GridSplitter splitter = sender as GridSplitter;
+
+            var rowDefs = _PreviewGrid.RowDefinitions;
+            var colDefs = _PreviewGrid.ColumnDefinitions;
+
+            var w0 = colDefs[0].ActualWidth;
+            var w1 = colDefs[1].ActualWidth;
+            var w2 = colDefs[2].ActualWidth;
+            var h0 = rowDefs[0].ActualHeight;
+            var h1 = rowDefs[1].ActualHeight;
+            var h2 = rowDefs[2].ActualHeight;
+
+            double W0 = w0, W1 = w1, W2 = w2, H0 = h0, H1 = h1, H2 = h2;
+
+            const double Min = 5;
+
+            if (splitter.VerticalAlignment == VerticalAlignment.Top)
+            {
+                H0 = h0 + h1 - w1;
+                if (H0 < 0)
+                    H0 = Min;
+                H1 = h0 + h1 - H0;
+            }
+            else if (splitter.VerticalAlignment == VerticalAlignment.Bottom)
+            {
+                H2 = h2 + h1 - w1;
+                if (H2 < 0)
+                    H2 = Min;
+                H1 = h2 + h1 - H2;
+            }
+            else if (splitter.HorizontalAlignment == HorizontalAlignment.Left)
+            {
+                W0 = w0 + w1 - h1;
+                if (W0 < 0)
+                    W0 = Min;
+                W1 = w0 + w1 - W0;
+            }
+            else //(splitter.HorizontalAlignment == HorizontalAlignment.Right)
+            {
+                W2 = w2 + w1 - h1;
+                if (W2 < 0)
+                    W2 = Min;
+                W1 = w2 + w1 - W2;
+            }
+
+            // using 'Pixel' would mess with the functioning of the Splitter - those sizes will not change.
+            rowDefs[0].Height = new GridLength(H0, GridUnitType.Star);
+            rowDefs[1].Height = new GridLength(H1, GridUnitType.Star);
+            rowDefs[2].Height = new GridLength(H2, GridUnitType.Star);
+            colDefs[0].Width = new GridLength(W0, GridUnitType.Star);
+            colDefs[1].Width = new GridLength(W1, GridUnitType.Star);
+            colDefs[2].Width = new GridLength(W2, GridUnitType.Star);
+        }
     }
 }
